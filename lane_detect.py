79f6a4cb25a7a243e45d7897ccea600f080f2c13
smashkoala <keita.cool@gmail.com>
2017-05-15 07:08:06 +0200

    First commit
    - Added a new source code file
    - Added pickle files for camera calibrations

---
+++ lane_detect.py
@@ -0,0 +1,78 @@
+import pickle
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import numpy as np
+import cv2
+import glob
+import matplotlib.pyplot as plt
+#%matplotlib qt
+
+
+def prepare_calibration():
+    # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
+    objp = np.zeros((6*9,3), np.float32)
+    objp[:,:2] = np.mgrid[0:9, 0:6].T.reshape(-1,2)
+
+    # Arrays to store object points and image points from all the images.
+    objpoints = [] # 3d points in real world space
+    imgpoints = [] # 2d points in image plane.
+
+    # Make a list of calibration images
+    images = glob.glob('camera_cal/calibration*.jpg')
+    # Step through the list and search for chessboard corners
+    for idx, fname in enumerate(images):
+        img = cv2.imread(fname)
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+        # Find the chessboard corners
+        ret, corners = cv2.findChessboardCorners(gray, (9,6), None)
+        # If found, add object points, image points
+        if ret == True:
+            objpoints.append(objp)
+            imgpoints.append(corners)
+
+            Draw and display the corners
+            cv2.drawChessboardCorners(img, (9,6), corners, ret)
+            cv2.imshow('img', img)
+            cv2.waitKey(500)
+
+        cv2.destroyAllWindows()
+
+    calib_pickle = {}
+    calib_pickle["objpoints"] = objpoints
+    calib_pickle["imgpoints"] = imgpoints
+    pickle.dump( calib_pickle, open( "wide_dist_pickle.p", "wb" ))
+    return objpoints, imgpoints
+
+def test_images(objpoints, imgpoints):
+    # Test undistortion on an image
+    img = cv2.imread('test_images/test1.jpg')
+    img_size = (img.shape[1], img.shape[0])
+
+    # Do camera calibration given object points and image points
+    ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, img_size,None,None)
+
+    dst = cv2.undistort(img, mtx, dist, None, mtx)
+    cv2.imwrite('calibration_wide/test_undist.jpg',dst)
+
+    # Save the camera calibration result for later use (we won't worry about rvecs / tvecs)
+    dist_pickle = {}
+    dist_pickle["mtx"] = mtx
+    dist_pickle["dist"] = dist
+    pickle.dump( dist_pickle, open( "camera_calib_pickle.p", "wb" ) )
+    #dst = cv2.cvtColor(dst, cv2.COLOR_BGR2RGB)
+    # Visualize undistortion
+    cv2.imshow('img', img)
+    cv2.waitKey(1000)
+    cv2.imshow('dst', dst)
+    cv2.waitKey(1000)
+
+#objpoints, imgpoints = prepare_calibration()
+#test_images(objpoints, imgpoints)
+
+dist_pickle = pickle.load( open( "wide_dist_pickle.p", "rb" ) )
+objpoints = dist_pickle["objpoints"]
+imgpoints = dist_pickle["imgpoints"]
+test_images(objpoints, imgpoints)
